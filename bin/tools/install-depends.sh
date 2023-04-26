d7a7f0a2c1cf2cef3020d9144275c602f2357fe3
galaxyeye <ivincent.zhang@gmail.com>
2023-04-26 10:05:58 +0800

    Imps: 1. add script to install dependent packages.

---
+++ bin/tools/install-depends.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+install_utils() {
+  if ! command -v java &> /dev/null
+  then
+    sudo apt install wget vim default-jdk maven git curl
+  fi
+
+  if ! command -v mvn &> /dev/null
+  then
+      sudo apt install maven
+  fi
+
+  if ! command -v curl &> /dev/null
+  then
+      sudo apt install curl
+  fi
+}
+
+start_dependent_daemon() {
+  # "Failed to connect to bus: No such file or directory" on clean WSL
+  # https://github.com/Microsoft/WSL/issues/2941
+  uname=$(uname -a)
+  if [[ "$uname" == *"microsoft"* ]]
+  then
+    status=$(service dbus status)
+    if [[ "$status" != *"running"* ]]
+    then
+      sudo service dbus start
+    fi
+  fi
+}
+
+install_chrome() {
+    echo "Installing latest stable google-chrome"
+
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome*.deb
+    sudo apt-get install -f
+
+    google-chrome -version
+}
+
+cd /tmp/ || exit
+
+start_dependent_daemon
+
+# find out chrome version
+CHROME_VERSION="$(google-chrome -version | head -n1 | awk -F '[. ]' '{print $3}')"
+
+if [[ "$CHROME_VERSION" == "" ]]; then
+    install_chrome
+fi
+
+install_utils
+
+cd - || exit
