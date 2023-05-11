02cead1fc401bd5404befe0ac2da5d95207a0f90
galaxyeye <ivincent.zhang@gmail.com>
2023-05-11 11:08:13 +0800

    Imps: 1. add release scripts

---
+++ bin/release/deploy.sh
@@ -0,0 +1,18 @@
+#bin
+
+bin=$(dirname "$0")/..
+bin=$(cd "$bin">/dev/null || exit; pwd)
+APP_HOME=$(cd "$bin"/..>/dev/null || exit; pwd)
+
+echo "Deploy the project ..."
+echo "Changing version ..."
+
+SNAPSHOT_VERSION=$(head -n 1 "$APP_HOME/VERSION")
+VERSION=${SNAPSHOT_VERSION//"-SNAPSHOT"/""}
+echo "$VERSION" > "$APP_HOME"/VERSION
+find "$APP_HOME" -name 'pom.xml' -exec sed -i "s/$SNAPSHOT_VERSION/$VERSION/" {} \;
+
+mvn clean
+mvn deploy -Pplaton-deploy -DskipTests=true -DaltDeploymentRepository=local::default::file:./target/staging-deploy
+
+scp -r ./target/staging-deploy/* master:~/platonic.fun/repo
---
+++ bin/release/next-minor.sh
@@ -0,0 +1,47 @@
+#bin
+
+bin=$(dirname "$0")/..
+bin=$(cd "$bin">/dev/null || exit; pwd)
+APP_HOME=$(cd "$bin"/..>/dev/null || exit; pwd)
+
+SNAPSHOT_VERSION=$(head -n 1 "$APP_HOME/VERSION")
+VERSION=${SNAPSHOT_VERSION//"-SNAPSHOT"/""}
+PREFIX=$(echo "$VERSION" | cut -d'.' -f1,2)
+MINOR_VERSION=$(echo "$VERSION" | cut -d'.' -f3)
+MINOR_VERSION=$(("$MINOR_VERSION" + 1))
+
+NEXT_VERSION="$PREFIX.$MINOR_VERSION"
+NEXT_SNAPSHOT_VERSION="$NEXT_VERSION-SNAPSHOT"
+
+echo "New version: $NEXT_SNAPSHOT_VERSION"
+# VERSION file
+echo "$NEXT_SNAPSHOT_VERSION" > "$APP_HOME"/VERSION
+# $APP_HOME/pom.xml
+sed -i -e "s/<tag>v$VERSION<\/tag>/<tag>v$NEXT_VERSION<\/tag>/g" "$APP_HOME/pom.xml";
+# pom.xml files
+find "$APP_HOME" -name 'pom.xml' -exec sed -i "s/$SNAPSHOT_VERSION/$NEXT_SNAPSHOT_VERSION/" {} \;
+
+# The following files contains the version number to upgrade
+VERSION_AWARE_FILES=(
+  "$APP_HOME/README.adoc"
+  "$APP_HOME/README.md"
+  "$APP_HOME/README-CN.adoc"
+  "$APP_HOME/README-CN.md"
+)
+# replace version numbers to be the next numbers in files
+for F in "${VERSION_AWARE_FILES[@]}"; do
+  if [ -e "$F" ]; then
+    sed -i "s/\b$PREFIX.[0-9]\{1,\}\b/$NEXT_VERSION/g" "$F";
+  fi
+done
+
+COMMENT=${NEXT_SNAPSHOT_VERSION//"-SNAPSHOT"/""}
+
+echo "Ready to commit with comment: <$COMMENT>"
+read -p "Are you sure to continue? [Y/n]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git add .
+  git commit -m "$COMMENT"
+  git push
+fi
